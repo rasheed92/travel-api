213f3777f8994e7587a7a75833a08a0102de3591
rasheed92 <emperor.rasheed@gmail.com>
2019-02-07 20:57:21 +0300

    Update ApiReqDataController.php

--- app/Http/Controllers/ApiReqDataController.php
+++ app/Http/Controllers/ApiReqDataController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
-class ApiRequestController extends Controller
+class ApiReqDataController extends Controller
 {
     public function getApiData(Request $Request)
     {
